1fe71a5e8d493a5d0e41202d6f99fba3d5ceead8
Samara Martins <115640218+SamaraMartinss@users.noreply.github.com>
2023-11-29 21:48:11 -0300

    atualização do codigo
    adicionei a AdmView e complementei o cadastrar funcionario da classe funcionario

--- src/main/java/com/mycompany/Model/Funcionario.java
+++ src/main/java/com/mycompany/Model/Funcionario.java
@@ -33,6 +33,14 @@ public class Funcionario extends Pessoa {
         this.setEmail(email);
         this.setSenha(password);
     }
-    
+
+    public Funcionario(String nome, CPF cpf, Email email, String senha, String cargo, int id){ 
+        this.setNome(nome);
+        this.setCpf(cpf);
+        this.setEmail(email);
+        this.setSenha(senha);
+        this.setCargo(cargo);
+        this.setId(id);
+    }
     public Funcionario(){}
 }
---
+++ src/main/java/com/mycompany/View/AdmView.java
@@ -0,0 +1,92 @@
+package com.mycompany.View;
+
+import java.util.Scanner;
+import com.mycompany.Controller.*;
+import com.mycompany.Model.*;
+
+public class AdmView {   
+    
+    private Administrador adm; 
+    
+    public AdmView(Administrador adm){   // metodo construtor
+        this.adm = adm;
+    }
+
+    public void mostrarOpcoes(){
+        try(Scanner scanner = new Scanner(System.in)){
+            int opcao;
+
+            do{
+                System.out.println("*******************************************************************");
+                System.out.println("Escolha uma opção:");
+                System.out.println("1. Cadastrar Funcionario");
+                System.out.println("2. Alterar Funcionario");
+                System.out.println("3. Remover Funcionario");
+                System.out.println("4. Validar Requisição");
+                System.out.println("0. Sair");
+                System.out.println("*******************************************************************");
+               
+                opcao = scanner.nextInt();
+                scanner.nextLine();
+                switch (opcao) {
+                    case 1:
+                    
+                    String nome, senha, cargo, cpf_String, email_String;
+                    int id;
+                    CPF cpf;
+                    Email email;
+                    
+                    System.out.println("digite o id do funcionario:");
+                    id = scanner.nextInt();
+                    
+                    System.out.println("digite o nome do funcionario:");
+                    nome = scanner.nextLine();
+                    
+                    System.out.println("digite o CPF do funcionario:");
+                    cpf_String = scanner.nextLine();
+                    cpf = new CPF(cpf_String);
+
+                    System.out.println("digite o Email do funcionario:");
+                    email_String = scanner.nextLine();
+                    email = new Email(email_String);
+
+                    System.out.println("digite a senha do funcionario:");
+                    senha = scanner.nextLine();
+
+                    System.out.println("digite o cargo do funcionario:");
+                    cargo = scanner.nextLine();
+
+                    Funcionario funcionario = new Funcionario(nome, cpf, email, senha, cargo, id);
+
+                    adm.cadastrarFuncionario(funcionario);
+                    break;
+                case 2:
+                    String nome_alterar = adm.getNome();
+                    CPF cpf_alterar = adm.getCpf();
+                    Email email_alterar = adm.getEmail();
+                    String senha_alterar = adm.getSenha();
+                    String cargo_alterar = adm.getCargo();
+                    adm.alterarFuncionario(nome_alterar, cpf_alterar, email_alterar, senha_alterar, cargo_alterar);
+                    break;
+                case 3:
+                    int id_remover = adm.getId();
+                    adm.removerFuncionario(id_remover);
+                    break;
+                case 4:
+                    adm.validarRequisicao();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
+
+                }
+            
+            } while (opcao != 0);
+        }
+    
+    }
+
+}
