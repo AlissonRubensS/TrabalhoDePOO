7be711df245fce7b214077800dad4a28b3a2b648
Alisson Rubens <alissonrubens.s@gmail.com>
2023-12-04 15:36:45 -0300

    xx
    a

--- src/main/java/com/mycompany/Controller/TecAdmController.java
+++ src/main/java/com/mycompany/Controller/TecAdmController.java
@@ -36,19 +36,38 @@ public class TecAdmController {
             chamado.setNovaDescricao(aux[4]);
             
             listaChamados.add(chamado);
-
         }
+public Requisicao getRequisicao(int id){
 
-        return listaChamados;
+    ArrayList <String> content = File.read("../Database/Requisicao.txt");
+    for (int i = 0; i<content.size(); i++){
+        String id2String[] = content.get(i).split("-=-");
+        if (id==Integer.parseInt(id2String[0])) {
+            for(String line : content){
+                String[] aux = line.split("-=-");
+                
+                Requisicao tempreq = new Requisicao();
+                tempreq.setId(Integer.parseInt(aux[0]));
+                Data data = new Data(aux[1]);
+                tempreq.setData_requisicao(data);
+
+                tempreq.setLocal(aux[2]);
+                tempreq.setObjeto(aux[3]);
+                tempreq.setStatus(aux[4]);
+                tempreq.setNivelUrgencia(aux[5]);
+
+                return tempreq;
+            }
+        }
     }
-    
+    return null;
+}
     public static ArrayList<Requisicao> carregar_requisicoes(){
         ArrayList<Requisicao> listaRequicicoes = new ArrayList<Requisicao>();
         ArrayList<String> linhas = File.read("src\\main\\java\\com\\mycompany\\Database\\Requisicoes.txt");
 
         for (String linha : linhas) {
             String[] aux = linha.split("-=-");
-
             Requisicao requisicao = new Requisicao();
             requisicao.setId(Funcionario.geraid());
 
